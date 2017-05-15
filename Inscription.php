b3f76cc473b657c9c5a8af31841db23ffa9a1f4c
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-15 23:09:05 +0200

    gestion accueil

--- Inscription.php
+++
@@ -1,3 +0,0 @@
-<?php
-	require_once("controller/Controller_Page_Inscription.php");
-?>
---
+++ controller/Controller_Page_Accueil.php
@@ -0,0 +1,8 @@
+<?php
+//require_once ("model/User.php");
+
+//require_once ("controller/Controller_Test_User.php");
+
+require "view/accueil.php";
+
+?>
