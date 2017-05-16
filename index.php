ae93fba9706f88b5293bfd2673e923e1ab392860
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-16 09:13:58 +0200

    début de chemin

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-require_once ("Accueil.php");
+require_once("Accueil.php");
+	//header('Location:controller/Controller_Page_Accueil.php');
 ?>
--- view/accueil.php
+++ view/accueil.php
@@ -10,7 +10,7 @@
     <meta name="Author" content="Amin BAZAZ"> 
       <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
       <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"> 
-      <link rel="stylesheet" href="C:\Users\amin\Desktop\Web\css\materialize\css\materialize.css"> 
+      <link rel="stylesheet" href="css/materialize/css/materialize.css"> 
   </head> 
  
   <body> 
@@ -18,7 +18,7 @@
           <div class="nav-wrapper"> 
             <a href="#" class="brand-logo">CENTRE SOCIAL DE NAILLAC</a> 
             <ul id="nav-mobile" class="right hide-on-med-and-down"> 
-              <li><a href="sass.html">Connection</a></li> 
+              <li><a href="sass.html">Connexion</a></li> 
               <li><a href="badges.html">Inscription</a></li> 
             </ul> 
           </div> 
