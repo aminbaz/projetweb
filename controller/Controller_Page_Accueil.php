5925507c54ff2f19933d17ffb0c80a2aef7a1b55
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-24 15:49:36 +0200

    require

--- controller/Controller_Page_Accueil.php
+++ controller/Controller_Page_Accueil.php
@@ -6,11 +6,16 @@
 //onlineonly();
 
 
+
+require_once "view/accueil.php";
+
+//require_once ("controller/Controller_Test_Utilisateur.php");
+
+//onlineonly();
+
 require_once ("model/Message.php");
 
 $information = Message::Get_Message();
-
-require_once "view/accueil.php";
 
 
 
