233dc85d02ccf22e557519bd16b3c38141e88a7c
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-24 14:15:24 +0200

    changemetn

--- controller/Controller_Page_Accueil.php
+++ controller/Controller_Page_Accueil.php
@@ -1,11 +1,11 @@
 <?php
-require_once ("model/Utilisateur.php");
-require_once ("model/Message.php");
+//require_once ("model/Utilisateur.php");
+//require_once ("model/Message.php");
 //require_once ("controller/Controller_Test_Utilisateur.php");
 
 //onlineonly();
 
-$information = Message::Get_Message();
+//$information = Message::Get_Message();
 
 require_once "view/accueil.php";
 
