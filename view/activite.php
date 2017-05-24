29ed9d211601ce3d5a75b8e96269d95cbc90520d
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-24 22:58:02 +0200

    selected

--- view/activite.php
+++ view/activite.php
@@ -49,14 +49,23 @@
           </div>
 
 
+          <div class="row">
+            <div class="input-field col s9">
+              <select>
+                <option value="" disabled selected>Choose your option</option>
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
+              </select>
+              <label>Materialize Select</label>
+            </div>
+          </div>  
 
           <div class="row">
-            <select>
-              <option value="" disabled selected>Choose your option</option>
-              <option value="1">Option 1</option>
-              <option value="2">Option 2</option>
-              <option value="3">Option 3</option>
-            </select>
+            <div class="input-field col s9">
+              <input id="typeactivite" name="typeactivite" type="text" class="validate">
+              <label for="typeactivite" data-error="wrong" data-success="right">Type</label>
+            </div>
           </div>
 
           <div class="row">
