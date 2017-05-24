a2f86e8c4a0b8d5edc3802842301b9fd28b67045
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-24 11:11:36 +0200

    debut deploement

--- controller/Controller_Desinscription_Activite.php
+++ controller/Controller_Desinscription_Activite.php
@@ -3,6 +3,8 @@
 	onlineOnly();
 	require_once ('/model/Inscrire.php');
 	$idUtilisateur = $_COOKIE['codeconnexion'];
+
+	
 	$idActivite = $_GET['id_activite'];
 	Inscrire::Delete_Inscription($idUtilisateur,$idActivite);
 	header("Location: Activite.php");
