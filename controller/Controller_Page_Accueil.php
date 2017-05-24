303502f1eee222311e1d014bd037ca6b97a84cfb
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-24 16:37:31 +0200

    suppression d'une contrainte en ligne

--- controller/Controller_Page_Accueil.php
+++ controller/Controller_Page_Accueil.php
@@ -6,8 +6,6 @@ require_once ("model/Utilisateur.php");
 
 require_once ("/controller/Controller_Test_Connexion.php");
 
-onlineonly();
-
 require_once ("model/Message.php");
 
 $information = Get_Message();
