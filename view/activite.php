ea3317a85e0c505ac90130b23955cf482429ba33
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-24 23:01:10 +0200

    selected

--- view/activite.php
+++ view/activite.php
@@ -49,17 +49,14 @@
           </div>
 
 
-          <div class="row">
-            <div class="input-field col s9">
-              <select>
-                <option value="" disabled selected>Choose your option</option>
-                <option value="1">Option 1</option>
-                <option value="2">Option 2</option>
-                <option value="3">Option 3</option>
-              </select>
-              <label>Materialize Select</label>
-            </div>
-          </div>  
+          <div class="input-field col s12">
+            <select name="gender" id="gender">
+              <option value="" disabled selected>Entrer votre sexe</option>
+              <option value="Homme">Homme</option>
+              <option value="Femme">Femme</option>
+            </select>
+            <label for="gender">Sexe</label>
+          </div>    
 
           <div class="row">
             <div class="input-field col s9">
