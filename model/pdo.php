d2f44704b91fbf029bb8970ec229e933f64cdb85
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-24 17:26:03 +0200

    bd

--- model/pdo.php
+++ model/pdo.php
@@ -1,14 +1,17 @@
 <?php
 function connexion()
 {
+	$dbname = 'd5q4pq6s1eblro';
+	$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
+	$dbuser = 'bjeocquzxzeuqh';
+	$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
+
 	try
 	{
-		$pseudo='bjeocquzxzeuqh';
-		$password='70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-		$pdo = new PDO('pgsql:host=ec2-54-247-166-129.eu-west-1.compute.amazonaws.com;dbname=d5q4pq6s1eblro',$pseudo,$password);
+		$pdo = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
-	catch (Exception $e)
+	catch (PDOException $e)
 	{
 			die('Erreur : ' . $e->getMessage());
 	}
