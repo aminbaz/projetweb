e1ad8049f361eb04850e1a4fc147a4d8e639ef23
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-24 17:13:45 +0200

    base de donnée

--- model/pdo.php
+++ model/pdo.php
@@ -3,9 +3,9 @@ function connexion()
 {
 	try
 	{
-		$pseudo='postgres';
-		$password='postgres';
-		$pdo = new PDO('pgsql:host=localhost;dbname=centre',$pseudo,$password);
+		$pseudo='bjeocquzxzeuqh';
+		$password='70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
+		$pdo = new PDO('pgsql:host=ec2-54-247-166-129.eu-west-1.compute.amazonaws.com;dbname=d5q4pq6s1eblro',$pseudo,$password);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch (Exception $e)
