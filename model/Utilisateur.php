4b6d9132d0fc42f0ed8e8c3a8680654bc17ba45e
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-24 18:09:31 +0200

    modif user

--- model/Utilisateur.php
+++ model/Utilisateur.php
@@ -1,8 +1,9 @@
 <?php
-
+	require_once('Pdo.php');
+	$bd=connexion();
 			function Get_User($userId){
-				require_once('Pdo.php');
-				$bd=connexion();
+				
+				
 
 				$req = $bd->prepare("SELECT * FROM utilisateur WHERE id_utilisateur = :userId");
 				$req->bindParam(':userId',$userId);
@@ -18,8 +19,8 @@
 			//données : $userPassword string correspondant au mot de passe utilisateur, $userMail string correspondant au mail de l'utilisateur
 			//résultat : bool vérifiant si le mot de passe entré correspond bien au mail de l'utilisateur
 			{
-				require_once('Pdo.php');
-				$bd=connexion();
+			
+				
 			   //connecté à la base de donnée 
 
 				$req = $bd->prepare("SELECT mdp FROM utilisateur WHERE mail='".$userMail."'");
@@ -36,8 +37,7 @@
 			//données : $userCookieCode string correspondant à un code cookie
 			//résultat : vérifie si un code cookie existe dans la base de données, et le cas échéant renvoie un int correspondant à l'id de l'utilisateur auquel appartient le code cookie
 			{
-				require_once('Pdo.php');
-				$bd=connexion();
+				
 
 
 				$req = $bd->prepare("SELECT id_utilisateur FROM utilisateur WHERE mail='".$userMail."'");
@@ -53,8 +53,7 @@
 			//données : string correspondant au mail à vérifier
 			//résultat : renvoie un booleen pour savoir si le mail est déja utilisé
 			{
-				require_once('Pdo.php');
-				$bd=connexion();
+				
 
 				$req = $bd->prepare("SELECT * FROM utilisateur WHERE mail='".$mail."'");
 				
@@ -69,8 +68,7 @@
 			//données : $gender string correspondant au sexe de l'utilisateur à ajouter, $name string correspondant au nom de l'utilisateur, $firstName string correspondant au prénom de l'utilisateur, $password string correspondant au mot de passe de l'utilisateur, $mail string correspondant au mail de l'utilisateur
 			//résultat : modifie la base de données en ajoutant une entité à la classe "User" en fonction des données entrées
 			{
-				require_once('Pdo.php');
-				$bd=connexion();
+				
 				$age = (time() - strtotime($datenaiss)) / 3600 / 24 / 365;
 				if ($age < 11) {
 					$categorie = 1;
@@ -98,8 +96,7 @@
 
 			function Set_User_Role($userId,$roleId)
 			{
-				require_once('Pdo.php');
-				$bd=connexion();
+				
 
 				$req = $bd->prepare("UPDATE Utilisateur SET id_role =:roleId WHERE id_utilisateur=:userId");
 				$req->bindParam(':roleId',$roleId);
@@ -113,8 +110,7 @@
 			//données : $userId int correspondant à l'identifiant de l'utilisateur
 			//résultat : int correspondant à l'id du rôle de l'utilisateur
 			{
-				require_once('Pdo.php');
-				$bd=connexion();
+			
 
 				$req = $bd->prepare("SELECT id_role FROM utilisateur WHERE id_utilisateur = :userId");
 				$req->bindParam(':userId',$userId);
@@ -129,8 +125,7 @@
 			//données : $userId int correspondant à l'identifiant de l'utilisateur
 			//résultat : int correspondant à l'id du rôle de l'utilisateur
 			{
-				require_once('Pdo.php');
-				$bd=connexion();
+				
 
 				$req = $bd->prepare("SELECT id_categorie FROM utilisateur WHERE id_utilisateur = :userId");
 				$req->bindParam(':userId',$userId);
@@ -145,8 +140,7 @@
 			//données : $userId int correspondant à l'identifiant de l'utilisateur
 			//résultat : int correspondant à l'id du rôle de l'utilisateur
 			{
-				require_once('Pdo.php');
-				$bd=connexion();
+				
 
 				$req = $bd->prepare("SELECT A.id_activite, A.nom_activite, A.description, A.prix, A.date_activite FROM activite A, inscrire I, utilisateur U WHERE A.id_activite = I.id_activite and I.id_utilisateur = U.id_utilisateur and U.id_utilisateur = :userId ");
 				$req->bindParam(':userId',$userId);
@@ -160,8 +154,7 @@
 			//données : $userId int correspondant à l'identifiant de l'utilisateur
 			//résultat : int correspondant à l'id du rôle de l'utilisateur
 			{
-				require_once('Pdo.php');
-				$bd=connexion();
+				
 
 				$req = $bd->prepare("SELECT A.id_activite, A.nom_activite, A.description, A.prix, A.date_activite FROM activite A, encadrer E, utilisateur U WHERE A.id_activite = E.id_activite and E.id_utilisateur = U.id_utilisateur and U.id_utilisateur = :userId ");
 				$req->bindParam(':userId',$userId);
@@ -176,8 +169,7 @@
 			//données : $userId int correspondant à l'identifiant de l'utilisateur
 			//résultat : int correspondant à l'id du rôle de l'utilisateur
 			{
-				require_once('Pdo.php');
-				$bd=connexion();
+				
 
 				$req = $bd->prepare("SELECT * FROM activite A, encadrer E, utilisateur U WHERE A.id_activite = E.id_activite and E.id_utilisateur = U.id_utilisateur and A.id_activite = :idActivite ");
 				$req->bindParam(':idActivite',$idActivite);
@@ -190,8 +182,7 @@
 
 
 			function Update_User($id,$name,$firstName,$gender,$datenaiss){
-				require_once('Pdo.php');
-				$bd=connexion();
+				
 
 				$req = $bd->prepare('UPDATE utilisateur set nom=:name, prenom=:firstname, sexe=:gender, date_naiss=:datenaiss where id_utilisateur=:id');
 				$req->bindParam(':id',$id);
@@ -205,8 +196,7 @@
 			}
 
 			function Update_Password($id,$mdp){
-				require_once('Pdo.php');
-				$bd=connexion();
+			
 
 				$req = $bd->prepare('UPDATE Utilisateur set mdp=:mdp where id_utilisateur=:id');
 				$req->bindParam(':mdp',$mdp);
