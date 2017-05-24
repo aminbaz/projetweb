371a31b50f4729aa34f054e55706b3a28f5f1d93
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-25 00:31:57 +0200

    test

--- model/Utilisateur.php
+++ model/Utilisateur.php
@@ -99,24 +99,8 @@
 			//données : string correspondant au mail à vérifier
 			//résultat : renvoie un booleen pour savoir si le mail est déja utilisé
 			{
-				//require_once('Pdo.php');
-				//$bd=connexion();
-
-				$dbname = 'd5q4pq6s1eblro';
-			$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
-			$dbuser = 'bjeocquzxzeuqh';
-			$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-
-			try
-			{
-				$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
-				$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			}
-			catch (PDOException $e)
-			{
-					echo $e->getMessage();
-					die('<br> Echec lors de la connexion à la BD');
-			}
+				require_once('Pdo.php');
+				$bd=connexion();
 
 				$req = $bd->prepare("SELECT * FROM utilisateur WHERE mail='".$mail."'");
 				
