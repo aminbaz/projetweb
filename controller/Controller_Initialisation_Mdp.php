405691d03b9feba78bd47aecfd350ad5cec9f654
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-25 10:47:43 +0200

    init mdp

--- controller/Controller_Initialisation_Mdp.php
+++ controller/Controller_Initialisation_Mdp.php
@@ -1,5 +1,5 @@
 <?php
-	require_once ('../model/Utilisateur.php');
+	require_once ('model/Utilisateur.php');
 		$id_utilisateur = $_GET['id_utilisateur'];
 		Update_Password($id_utilisateur,'maison');
 	}
