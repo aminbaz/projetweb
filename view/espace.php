38da67c1d3ba0a1e322f10718086efd7f68259ae
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-25 17:27:57 +0200

    responsive

--- view/espace.php
+++ view/espace.php
@@ -6,49 +6,53 @@
   <?php require ("view/entete.php");?>
 	 <div class="container">
       <div class="row z-depth-4 grey lighten-5">
-        <form class="col s12" method="post" action="controller/Controller_Role.php">  
-        <h5 class="center-align">Attribution rôle</h5>        
-          <div class="row">
-            <div class="input-field col s9">
-              <input id="email" name="email" type="email" class="validate">
-              <label for="email" data-error="wrong" data-success="right">Email</label>
+        <form  method="post" action="controller/Controller_Role.php">  
+        <div class="col m6">
+          <h5 class="center-align">Attribution rôle</h5>        
+            <div class="row">
+              <div class="input-field col s9">
+                <input id="email" name="email" type="email" class="validate">
+                <label for="email" data-error="wrong" data-success="right">Email</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s9">
+                <input id="role" name="role" type="text" class="validate">
+                <label for="role" data-error="wrong" data-success="right">Role</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s9">
+                <button class="btn waves-effect waves-light grey" type="submit" name="action" value="valider">Changement</button>
+              </div>
             </div>
           </div>
-          <div class="row">
-            <div class="input-field col s9">
-              <input id="role" name="role" type="text" class="validate">
-              <label for="role" data-error="wrong" data-success="right">Role</label>
+        </form>
+      
+    
+
+   
+     
+        <form  method="post" action="Modification_Utilisateur.php">
+         <div class="col m6">
+          <h5 class="center-align">Changement donnée d'un utilisateur</h5>
+            <div class="row">
+              <div class="input-field col s9">
+                <input id="email" name="email" type="email" class="validate">
+                <label for="email" data-error="wrong" data-success="right">Email</label>
+              </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="input-field col s9">
-              <button class="btn waves-effect waves-light grey" type="submit" name="action" value="valider">Changement</button>
+            <div class="row">
+              <div class="input-field col s9">
+                <button class="btn waves-effect waves-light grey" type="submit" name="action" value="valider">Changement</button>
+              </div>
             </div>
           </div>
         </form>
       </div>
-    </div>
+    
 
-   <div class="container">
-      <div class="row z-depth-4 grey lighten-5">
-        <form class="col s12" method="post" action="Modification_Utilisateur.php">
-        <h5 class="center-align">Changement donnée d'un utilisateur</h5>
-          <div class="row">
-            <div class="input-field col s9">
-              <input id="email" name="email" type="email" class="validate">
-              <label for="email" data-error="wrong" data-success="right">Email</label>
-            </div>
-          </div>
-          <div class="row">
-            <div class="input-field col s9">
-              <button class="btn waves-effect waves-light grey" type="submit" name="action" value="valider">Changement</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-
-    <div class="container">
+    
       <div class="row z-depth-4 grey lighten-5">
         <form class="col s12" method="post" action="Publication.php">
         <h5 class="center-align">Publier un message</h5>
