6566c6f89940a31b5acb2841e7c2a0fb29530906
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-25 20:21:11 +0200

    redirection

--- model/Encadrer.php
+++ model/Encadrer.php
@@ -1,28 +1,9 @@
 <?php
 
-	function Add_Encadrement($idUser,$idActivite)
-				//User_Gender x User_Name x User_First_Name x User_Password x User_Mail =>
-				//données : $gender string correspondant au sexe de l'utilisateur à ajouter, $name string correspondant au nom de l'utilisateur, $firstName string correspondant au prénom de l'utilisateur, $password string correspondant au mot de passe de l'utilisateur, $mail string correspondant au mail de l'utilisateur
-				//résultat : modifie la base de données en ajoutant une entité à la classe "User" en fonction des données entrées
-				{
-					//require_once('Pdo.php');
-					//$bd=connexion();
-
-					$dbname = 'd5q4pq6s1eblro';
-					$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
-					$dbuser = 'bjeocquzxzeuqh';
-					$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-
-					try
-					{
-						$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
-						$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					}
-					catch (PDOException $e)
-					{
-							echo $e->getMessage();
-							die('<br> Echec lors de la connexion à la BD');
-					}
+	function Add_Encadrement($idUser,$idActivite){
+					
+					require_once('pdo.php');
+					$bd=connexion();
 					
 					$req = $bd->prepare('INSERT INTO encadrer(id_utilisateur, id_activite) VALUES (:idUser, :idActivite)');
 					$req->bindParam(':idUser',$idUser);
@@ -31,32 +12,12 @@
 					$req->execute();
 				}
 
-	function Delete_Encadrement($idUser,$idActivite)
-				//User_Gender x User_Name x User_First_Name x User_Password x User_Mail =>
-				//données : $gender string correspondant au sexe de l'utilisateur à ajouter, $name string correspondant au nom de l'utilisateur, $firstName string correspondant au prénom de l'utilisateur, $password string correspondant au mot de passe de l'utilisateur, $mail string correspondant au mail de l'utilisateur
-				//résultat : modifie la base de données en ajoutant une entité à la classe "User" en fonction des données entrées
-				{
-					//require_once('Pdo.php');
-					//$bd=connexion();
-
-					$dbname = 'd5q4pq6s1eblro';
-					$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
-					$dbuser = 'bjeocquzxzeuqh';
-					$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-
-					try
-					{
-						$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
-						$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					}
-					catch (PDOException $e)
-					{
-							echo $e->getMessage();
-							die('<br> Echec lors de la connexion à la BD');
-					}
+	function Delete_Encadrement($idUser,$idActivite){
+					
+					require_once('pdo.php');
+					$bd=connexion();
 					
 					$req = $bd->prepare('DELETE FROM encadrer WHERE id_utilisateur=:idUser and id_activite=:idActivite');
-
 					$req->bindParam(':idUser',$idUser);
 					$req->bindParam(':idActivite',$idActivite);
 
@@ -64,29 +25,12 @@
 				}
 
 
-	function Check_Encadrement($idUser,$idActivite)
-				{
-					//require_once('Pdo.php');
-					//$bd=connexion();
-
-					$dbname = 'd5q4pq6s1eblro';
-					$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
-					$dbuser = 'bjeocquzxzeuqh';
-					$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-
-					try
-					{
-						$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
-						$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					}
-					catch (PDOException $e)
-					{
-							echo $e->getMessage();
-							die('<br> Echec lors de la connexion à la BD');
-					}
-
-					$req = $bd->prepare('SELECT * FROM encadrer WHERE id_utilisateur=:idUser and id_activite=:idActivite');
+	function Check_Encadrement($idUser,$idActivite){
 					
+					require_once('pdo.php');
+					$bd=connexion();
+
+					$req = $bd->prepare('SELECT * FROM encadrer WHERE id_utilisateur=:idUser and id_activite=:idActivite');				
 					$req->bindParam(':idUser',$idUser);
 					$req->bindParam(':idActivite',$idActivite);
 
@@ -98,27 +42,11 @@
 
 	function Check_Deja_Encadre($idActivite)
 				{
-					//require_once('Pdo.php');
-					//$bd=connexion();
-
-					$dbname = 'd5q4pq6s1eblro';
-					$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
-					$dbuser = 'bjeocquzxzeuqh';
-					$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-
-					try
-					{
-						$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
-						$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					}
-					catch (PDOException $e)
-					{
-							echo $e->getMessage();
-							die('<br> Echec lors de la connexion à la BD');
-					}
-
-					$req = $bd->prepare('SELECT id_utilisateur FROM encadrer WHERE id_activite=:idActivite');
 					
+					require_once('pdo.php');
+					$bd=connexion();
+
+					$req = $bd->prepare('SELECT id_utilisateur FROM encadrer WHERE id_activite=:idActivite');					
 					$req->bindParam(':idActivite',$idActivite);
 
 					$req->execute();
