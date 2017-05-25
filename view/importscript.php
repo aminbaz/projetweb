7000266e3a9e673f67988756818e6b091c6be9c7
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-25 16:36:32 +0200

    test

--- view/importscript.php
+++ view/importscript.php
@@ -9,5 +9,12 @@
 
 
       <script> $(document).ready(function() {
-    $('select').material_select();
-  });</script>
+	    $('select').material_select();
+	  });
+	  </script>
+
+	  <script>$('.datepicker').pickadate({
+	    selectMonths: true, // Creates a dropdown to control month
+	    selectYears: 15 // Creates a dropdown of 15 years to control year
+	  });</script>
+      
