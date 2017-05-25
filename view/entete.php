f0b002e1b8f44031d9be3855f0a0d073fb9d90d3
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-25 17:16:11 +0200

    responsive

--- view/entete.php
+++ view/entete.php
@@ -1,22 +1,4 @@
 <?php require_once ("controller/Controller_Test_Connexion.php");?>
-<!--<nav>
-	<div class="nav-wrapper grey">
-		<a href="Accueil.php" class="brand-logo left">Centre social de Naillac</a>
-		<ul id="nav-mobile" class="right hide-on-med-and-down">
-			<li><a href="Accueil.php">Accueil</a></li>
-			<?php if(isConnected() and isAdmin()){?><li><a href="Espace.php">Gestion</a></li><?php } ?>
-			<?php if(isConnected()){?><li><a href="Profil.php">Mes informations</a></li><?php } ?>
-			<?php if(isConnected()){?><li><a href="Activite.php">Activité</a></li><?php } ?>
-			<?php if(isConnected() and !isAdmin()){?><li><a href="Activite_Spec.php">Mes activités</a></li><?php } ?>
-			<?php if(!isConnected()){?><li><a href="Connexion.php">Connexion</a></li><?php } ?>
-			<?php if(!isConnected()){?><li><a href="Inscription.php">Inscription</a></li><?php } ?>
-			<?php if(isConnected()){?><li><a href="Deconnexion.php">Deconnexion</a></li><?php } ?>
-		</ul>
-	</div>
-</nav>
-<br>-->
-
-
 
 
   <nav>
@@ -45,3 +27,5 @@
       </ul>
     </div>
   </nav>
+
+  <br>
