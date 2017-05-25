784ca74941f2501c38b0f61d06c724c3ef8eb929
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-25 11:17:10 +0200

    init mdp

--- controller/Controller_Initialisation_Mdp.php
+++ controller/Controller_Initialisation_Mdp.php
@@ -1,8 +1,12 @@
 <?php
 	require_once ('../model/Utilisateur.php');
 		$id_utilisateur = $_GET['id_utilisateur'];
-		$mdp = 'maison';
-		Update_Password($id_utilisateur,sha1(sha1($mdp));
+		$password = 'maison';
+
+		$mdp = sha1(sha1($password));
+		Update_Password($id_utilisateur,$mdp);
+
+
 		header("Location: ../Espace.php");
 	
 ?>
