f6f54a1b8fd43cb2452848c56a07e46720e8b672
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-25 17:58:38 +0200

    bd

--- model/Utilisateur.php
+++ model/Utilisateur.php
@@ -1,8 +1,23 @@
 <?php
-			require_once('pdo.php');
-			$bd=connexion();
-
+			
 			function Get_User($userId){
+				//require_once('Pdo.php');
+				//$bd=connexion();
+				$dbname = 'd5q4pq6s1eblro';
+				$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
+				$dbuser = 'bjeocquzxzeuqh';
+				$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
+
+				try
+				{
+					$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
+					$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				}
+				catch (PDOException $e)
+				{
+						echo $e->getMessage();
+						die('<br> Echec lors de la connexion à la BD');
+				}
 
 				$req = $bd->prepare("SELECT * FROM utilisateur WHERE id_utilisateur = :userId");
 				$req->bindParam(':userId',$userId);
@@ -14,8 +29,12 @@
 			}
 
 			function Check_Password($userPassword,$userMail)
+			//User_Password x User_Mail => bool
+			//données : $userPassword string correspondant au mot de passe utilisateur, $userMail string correspondant au mail de l'utilisateur
+			//résultat : bool vérifiant si le mot de passe entré correspond bien au mail de l'utilisateur
 			{
-
+				require_once('pdo.php');
+				$bd=connexion();
 				$req = $bd->prepare("SELECT mdp FROM utilisateur WHERE mail='".$userMail."'");
 
 				$req->execute();
@@ -26,17 +45,61 @@
 			}
 
 			function Get_User_Id($userMail)
+			//User_Cookie_Code => User_Id
+			//données : $userCookieCode string correspondant à un code cookie
+			//résultat : vérifie si un code cookie existe dans la base de données, et le cas échéant renvoie un int correspondant à l'id de l'utilisateur auquel appartient le code cookie
 			{
+				//require_once('Pdo.php');
+				//$bd=connexion();
+				$dbname = 'd5q4pq6s1eblro';
+				$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
+				$dbuser = 'bjeocquzxzeuqh';
+				$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
+
+				try
+				{
+					$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
+					$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				}
+				catch (PDOException $e)
+				{
+						echo $e->getMessage();
+						die('<br> Echec lors de la connexion à la BD');
+				}
+
+
 				$req = $bd->prepare("SELECT id_utilisateur FROM utilisateur WHERE mail='".$userMail."'");
 
 				$req->execute();
 				$data=$req->fetch();
 
-				return $data["id_utilisateur"];
+				return $data["id_utilisateur"]; //Verifier si null
 			}
 
 			function Check_Mail($mail)
+			//User_Mail => [User]
+			//données : string correspondant au mail à vérifier
+			//résultat : renvoie un booleen pour savoir si le mail est déja utilisé
 			{
+				//require_once('Pdo.php');
+				//$bd=connexion();
+
+				$dbname = 'd5q4pq6s1eblro';
+			$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
+			$dbuser = 'bjeocquzxzeuqh';
+			$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
+
+			try
+			{
+				$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
+				$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}
+			catch (PDOException $e)
+			{
+					echo $e->getMessage();
+					die('<br> Echec lors de la connexion à la BD');
+			}
+
 				$req = $bd->prepare("SELECT * FROM utilisateur WHERE mail='".$mail."'");
 				
 				$req->execute();
@@ -46,7 +109,31 @@
 			}
 
 			function Add_Utilisateur($nom,$prenom,$sexe,$datenaiss,$mail,$mdp)
+			//User_Gender x User_Name x User_First_Name x User_Password x User_Mail =>
+			//données : $gender string correspondant au sexe de l'utilisateur à ajouter, $name string correspondant au nom de l'utilisateur, $firstName string correspondant au prénom de l'utilisateur, $password string correspondant au mot de passe de l'utilisateur, $mail string correspondant au mail de l'utilisateur
+			//résultat : modifie la base de données en ajoutant une entité à la classe "User" en fonction des données entrées
 			{
+				//require_once('Pdo.php');
+				//$bd=connexion();
+
+				$dbname = 'd5q4pq6s1eblro';
+				$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
+				$dbuser = 'bjeocquzxzeuqh';
+				$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
+
+				try
+				{
+					$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
+					$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				}
+				catch (PDOException $e)
+				{
+						echo $e->getMessage();
+						die('<br> Echec lors de la connexion à la BD');
+				}
+
+
+
 				$age = (time() - strtotime($datenaiss)) / 3600 / 24 / 365;
 				if ($age < 11) {
 					$categorie = 1;
@@ -74,6 +161,24 @@
 
 			function Set_User_Role($userId,$roleId)
 			{
+				//require_once('Pdo.php');
+				//$bd=connexion();
+				$dbname = 'd5q4pq6s1eblro';
+				$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
+				$dbuser = 'bjeocquzxzeuqh';
+				$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
+
+				try
+				{
+					$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
+					$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				}
+				catch (PDOException $e)
+				{
+						echo $e->getMessage();
+						die('<br> Echec lors de la connexion à la BD');
+				}
+
 				$req = $bd->prepare("UPDATE Utilisateur SET id_role =:roleId WHERE id_utilisateur=:userId");
 				$req->bindParam(':roleId',$roleId);
 				$req->bindParam(':userId',$userId);
@@ -82,7 +187,28 @@
 			}
 
 			function Get_User_Role($userId)
+			//User_Id => Role_Id
+			//données : $userId int correspondant à l'identifiant de l'utilisateur
+			//résultat : int correspondant à l'id du rôle de l'utilisateur
 			{
+				//require_once('Pdo.php');
+				//$bd=connexion();
+				$dbname = 'd5q4pq6s1eblro';
+				$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
+				$dbuser = 'bjeocquzxzeuqh';
+				$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
+
+				try
+				{
+					$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
+					$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				}
+				catch (PDOException $e)
+				{
+						echo $e->getMessage();
+						die('<br> Echec lors de la connexion à la BD');
+				}
+
 				$req = $bd->prepare("SELECT id_role FROM utilisateur WHERE id_utilisateur = :userId");
 				$req->bindParam(':userId',$userId);
 				$req->execute();
@@ -92,7 +218,28 @@
 			}
 
 			function Get_User_Categorie($userId)
+			//User_Id => Role_Id
+			//données : $userId int correspondant à l'identifiant de l'utilisateur
+			//résultat : int correspondant à l'id du rôle de l'utilisateur
 			{
+				//require_once('Pdo.php');
+				//$bd=connexion();
+				$dbname = 'd5q4pq6s1eblro';
+				$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
+				$dbuser = 'bjeocquzxzeuqh';
+				$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
+
+				try
+				{
+					$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
+					$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				}
+				catch (PDOException $e)
+				{
+						echo $e->getMessage();
+						die('<br> Echec lors de la connexion à la BD');
+				}
+
 				$req = $bd->prepare("SELECT id_categorie FROM utilisateur WHERE id_utilisateur = :userId");
 				$req->bindParam(':userId',$userId);
 				$req->execute();
@@ -102,7 +249,28 @@
 			}
 
 			function Get_Activite_Adherent($userId)
+			//User_Id => Role_Id
+			//données : $userId int correspondant à l'identifiant de l'utilisateur
+			//résultat : int correspondant à l'id du rôle de l'utilisateur
 			{
+				//require_once('Pdo.php');
+				//$bd=connexion();
+				$dbname = 'd5q4pq6s1eblro';
+				$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
+				$dbuser = 'bjeocquzxzeuqh';
+				$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
+
+				try
+				{
+					$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
+					$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				}
+				catch (PDOException $e)
+				{
+						echo $e->getMessage();
+						die('<br> Echec lors de la connexion à la BD');
+				}
+
 				$req = $bd->prepare("SELECT A.id_activite, A.nom_activite, A.description, A.prix, A.date_activite FROM activite A, inscrire I, utilisateur U WHERE A.id_activite = I.id_activite and I.id_utilisateur = U.id_utilisateur and U.id_utilisateur = :userId ");
 				$req->bindParam(':userId',$userId);
 				$req->execute();
@@ -111,7 +279,28 @@
 			}
 
 			function Get_Activite_Animateur($userId)
+			//User_Id => Role_Id
+			//données : $userId int correspondant à l'identifiant de l'utilisateur
+			//résultat : int correspondant à l'id du rôle de l'utilisateur
 			{
+				//require_once('Pdo.php');
+				//$bd=connexion();
+				$dbname = 'd5q4pq6s1eblro';
+				$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
+				$dbuser = 'bjeocquzxzeuqh';
+				$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
+
+				try
+				{
+					$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
+					$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				}
+				catch (PDOException $e)
+				{
+						echo $e->getMessage();
+						die('<br> Echec lors de la connexion à la BD');
+				}
+
 				$req = $bd->prepare("SELECT A.id_activite, A.nom_activite, A.description, A.prix, A.date_activite FROM activite A, encadrer E, utilisateur U WHERE A.id_activite = E.id_activite and E.id_utilisateur = U.id_utilisateur and U.id_utilisateur = :userId ");
 				$req->bindParam(':userId',$userId);
 				$req->execute();
@@ -121,7 +310,28 @@
 
 
 			function Get_Responsable_Activite($idActivite)
+			//User_Id => Role_Id
+			//données : $userId int correspondant à l'identifiant de l'utilisateur
+			//résultat : int correspondant à l'id du rôle de l'utilisateur
 			{
+				//require_once('Pdo.php');
+				//$bd=connexion();
+				$dbname = 'd5q4pq6s1eblro';
+				$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
+				$dbuser = 'bjeocquzxzeuqh';
+				$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
+
+				try
+				{
+					$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
+					$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				}
+				catch (PDOException $e)
+				{
+						echo $e->getMessage();
+						die('<br> Echec lors de la connexion à la BD');
+				}
+
 				$req = $bd->prepare("SELECT * FROM activite A, encadrer E, utilisateur U WHERE A.id_activite = E.id_activite and E.id_utilisateur = U.id_utilisateur and A.id_activite = :idActivite ");
 				$req->bindParam(':idActivite',$idActivite);
 				$req->execute();
@@ -133,6 +343,24 @@
 
 
 			function Update_User($id,$name,$firstName,$gender,$datenaiss){
+				//require_once('Pdo.php');
+				//$bd=connexion();
+				$dbname = 'd5q4pq6s1eblro';
+				$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
+				$dbuser = 'bjeocquzxzeuqh';
+				$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
+
+				try
+				{
+					$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
+					$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				}
+				catch (PDOException $e)
+				{
+						echo $e->getMessage();
+						die('<br> Echec lors de la connexion à la BD');
+				}
+
 				$req = $bd->prepare('UPDATE utilisateur set nom=:name, prenom=:firstname, sexe=:gender, date_naiss=:datenaiss where id_utilisateur=:id');
 				$req->bindParam(':id',$id);
 				$req->bindParam(':name',$name);
@@ -145,6 +373,24 @@
 			}
 
 			function Update_Password($id,$mdp){
+				//require_once('Pdo.php');
+				//$bd=connexion();
+				$dbname = 'd5q4pq6s1eblro';
+				$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
+				$dbuser = 'bjeocquzxzeuqh';
+				$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
+
+				try
+				{
+					$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
+					$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				}
+				catch (PDOException $e)
+				{
+						echo $e->getMessage();
+						die('<br> Echec lors de la connexion à la BD');
+				}
+
 				$req = $bd->prepare('UPDATE Utilisateur set mdp=:mdp where id_utilisateur=:id');
 				$req->bindParam(':mdp',$mdp);
 				$req->bindParam(':id',$id);
