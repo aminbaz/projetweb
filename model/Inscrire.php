bfda79ea4dc2c4f21a9a27f685421c7f84443956
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-25 20:27:10 +0200

    redirection

--- model/Inscrire.php
+++ model/Inscrire.php
@@ -1,28 +1,9 @@
 <?php
 
-	function Add_Inscription($idUser,$idActivite)
-				//User_Gender x User_Name x User_First_Name x User_Password x User_Mail =>
-				//données : $gender string correspondant au sexe de l'utilisateur à ajouter, $name string correspondant au nom de l'utilisateur, $firstName string correspondant au prénom de l'utilisateur, $password string correspondant au mot de passe de l'utilisateur, $mail string correspondant au mail de l'utilisateur
-				//résultat : modifie la base de données en ajoutant une entité à la classe "User" en fonction des données entrées
-				{
-					//require_once('Pdo.php');
-					//$bd=connexion();
-
-					$dbname = 'd5q4pq6s1eblro';
-					$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
-					$dbuser = 'bjeocquzxzeuqh';
-					$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-
-					try
-					{
-						$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
-						$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					}
-					catch (PDOException $e)
-					{
-							echo $e->getMessage();
-							die('<br> Echec lors de la connexion à la BD');
-					}
+	function Add_Inscription($idUser,$idActivite){
+					
+					require_once('pdo.php');
+					$bd=connexion();
 					
 					$req = $bd->prepare('INSERT INTO inscrire(id_utilisateur, id_activite) VALUES (:idUser, :idActivite)');
 					$req->bindParam(':idUser',$idUser);
@@ -31,32 +12,12 @@
 					$req->execute();
 				}
 
-	function Delete_Inscription($idUser,$idActivite)
-				//User_Gender x User_Name x User_First_Name x User_Password x User_Mail =>
-				//données : $gender string correspondant au sexe de l'utilisateur à ajouter, $name string correspondant au nom de l'utilisateur, $firstName string correspondant au prénom de l'utilisateur, $password string correspondant au mot de passe de l'utilisateur, $mail string correspondant au mail de l'utilisateur
-				//résultat : modifie la base de données en ajoutant une entité à la classe "User" en fonction des données entrées
-				{
-					//require_once('Pdo.php');
-					//$bd=connexion();
-
-					$dbname = 'd5q4pq6s1eblro';
-					$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
-					$dbuser = 'bjeocquzxzeuqh';
-					$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-
-					try
-					{
-						$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
-						$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					}
-					catch (PDOException $e)
-					{
-							echo $e->getMessage();
-							die('<br> Echec lors de la connexion à la BD');
-					}
+	function Delete_Inscription($idUser,$idActivite){
 					
-					$req = $bd->prepare('DELETE FROM inscrire WHERE id_utilisateur=:idUser and id_activite=:idActivite');
+					require_once('pdo.php');
+					$bd=connexion();
 
+					$req = $bd->prepare('DELETE FROM inscrire WHERE id_utilisateur=:idUser and id_activite=:idActivite');
 					$req->bindParam(':idUser',$idUser);
 					$req->bindParam(':idActivite',$idActivite);
 
@@ -66,27 +27,10 @@
 
 	function Check_Inscription($idUser,$idActivite)
 				{
-					//require_once('Pdo.php');
-					//$bd=connexion();
+					require_once('pdo.php');
+					$bd=connexion();
 
-					$dbname = 'd5q4pq6s1eblro';
-					$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
-					$dbuser = 'bjeocquzxzeuqh';
-					$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-
-					try
-					{
-						$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
-						$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					}
-					catch (PDOException $e)
-					{
-							echo $e->getMessage();
-							die('<br> Echec lors de la connexion à la BD');
-					}
-
-					$req = $bd->prepare('SELECT * FROM inscrire WHERE id_utilisateur=:idUser and id_activite=:idActivite');
-					
+					$req = $bd->prepare('SELECT * FROM inscrire WHERE id_utilisateur=:idUser and id_activite=:idActivite');				
 					$req->bindParam(':idUser',$idUser);
 					$req->bindParam(':idActivite',$idActivite);
 
@@ -98,24 +42,8 @@
 
 	function Nombre_Inscription($idActivite)
 				{
-					//require_once('Pdo.php');
-					//$bd=connexion();
-
-					$dbname = 'd5q4pq6s1eblro';
-					$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
-					$dbuser = 'bjeocquzxzeuqh';
-					$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-
-					try
-					{
-						$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
-						$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					}
-					catch (PDOException $e)
-					{
-							echo $e->getMessage();
-							die('<br> Echec lors de la connexion à la BD');
-					}
+					require_once('pdo.php');
+					$bd=connexion();
 
 					$req = $bd->prepare('SELECT count(*) FROM inscrire WHERE id_activite=:idActivite');
 					$req->bindParam(':idActivite',$idActivite);
--- model/Message.php
+++ model/Message.php
@@ -1,31 +1,11 @@
 <?php
 
-	function Add_Message($id,$titre,$texte,$date)
-			//User_Gender x User_Name x User_First_Name x User_Password x User_Mail =>
-			//données : $gender string correspondant au sexe de l'utilisateur à ajouter, $name string correspondant au nom de l'utilisateur, $firstName string correspondant au prénom de l'utilisateur, $password string correspondant au mot de passe de l'utilisateur, $mail string correspondant au mail de l'utilisateur
-			//résultat : modifie la base de données en ajoutant une entité à la classe "User" en fonction des données entrées
-			{
-				//require_once('Pdo.php');
-				//$bd=connexion();
+	function Add_Message($id,$titre,$texte,$date){
 
-					$dbname = 'd5q4pq6s1eblro';
-					$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
-					$dbuser = 'bjeocquzxzeuqh';
-					$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-
-					try
-					{
-						$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
-						$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					}
-					catch (PDOException $e)
-					{
-							echo $e->getMessage();
-							die('<br> Echec lors de la connexion à la BD');
-					}
+				require_once('pdo.php');
+				$bd=connexion();
 				
 				$req = $bd->prepare('INSERT INTO message(lib_message, texte_message, id_utilisateur, date_message) VALUES (:titre, :texte,:id,:dateMes)');
-
 				$req->bindParam(':id',$id);
 				$req->bindParam(':titre',$titre);
 				$req->bindParam(':texte',$texte);
@@ -35,80 +15,34 @@
 			}
 
 	function Get_Message(){
-				//require_once('Pdo.php');
-				//$bd=connexion();
 
-					$dbname = 'd5q4pq6s1eblro';
-					$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
-					$dbuser = 'bjeocquzxzeuqh';
-					$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-
-					try
-					{
-						$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
-						$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					}
-					catch (PDOException $e)
-					{
-							echo $e->getMessage();
-							die('<br> Echec lors de la connexion à la BD');
-					}
+				require_once('pdo.php');
+				$bd=connexion();
 
 				$req = $bd->prepare("SELECT * FROM message WHERE date_message > current_date - 5");
-
 				$req->execute();;
 
-				return $req; //Verifier si null
+				return $req;
 			}
 
 	function Get_All_Message($idMessage){
-				//require_once('Pdo.php');
-				//$bd=connexion();
-
-					$dbname = 'd5q4pq6s1eblro';
-					$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
-					$dbuser = 'bjeocquzxzeuqh';
-					$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-
-					try
-					{
-						$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
-						$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					}
-					catch (PDOException $e)
-					{
-							echo $e->getMessage();
-							die('<br> Echec lors de la connexion à la BD');
-					}
+				
+				require_once('pdo.php');
+				$bd=connexion();
 
 				$req = $bd->prepare("SELECT * FROM message WHERE id_message = :idMessage");
 				$req->bindParam(':idMessage',$idMessage);
 
 				$req->execute();
 
-				return $req; //Verifier si null
+				return $req;
 			}
 
 
 	function Update_Message($id,$titre,$texte,$date){
-				//require_once('Pdo.php');
-				//$bd=connexion();
-
-					$dbname = 'd5q4pq6s1eblro';
-					$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
-					$dbuser = 'bjeocquzxzeuqh';
-					$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-
-					try
-					{
-						$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
-						$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					}
-					catch (PDOException $e)
-					{
-							echo $e->getMessage();
-							die('<br> Echec lors de la connexion à la BD');
-					}
+				
+				require_once('pdo.php');
+				$bd=connexion();
 
 				$req = $bd->prepare('UPDATE message set lib_message=:titre, texte_message=:texte, date_message=:dateMes where id_message=:id');
 				$req->bindParam(':id',$id);
@@ -116,39 +50,18 @@
 				$req->bindParam(':texte',$texte);
 				$req->bindParam(':dateMes',$date);
 				
-
 				$req->execute();
 			}
 
-	function Delete_Message($id)
-				//User_Gender x User_Name x User_First_Name x User_Password x User_Mail =>
-				//données : $gender string correspondant au sexe de l'utilisateur à ajouter, $name string correspondant au nom de l'utilisateur, $firstName string correspondant au prénom de l'utilisateur, $password string correspondant au mot de passe de l'utilisateur, $mail string correspondant au mail de l'utilisateur
-				//résultat : modifie la base de données en ajoutant une entité à la classe "User" en fonction des données entrées
-				{
-					//require_once('Pdo.php');
-					//$bd=connexion();
+	function Delete_Message($id){
 
-					$dbname = 'd5q4pq6s1eblro';
-					$host = 'ec2-54-247-166-129.eu-west-1.compute.amazonaws.com';
-					$dbuser = 'bjeocquzxzeuqh';
-					$dbpass = '70e714e41ab10e7ad78a08d680301ffcc762857eace102ae3fcbcba1d150728d';
-
-					try
-					{
-						$bd = new PDO("pgsql:host=$host;dbname=$dbname",$dbuser,$dbpass);
-						$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					}
-					catch (PDOException $e)
-					{
-							echo $e->getMessage();
-							die('<br> Echec lors de la connexion à la BD');
-					}
+				require_once('pdo.php');
+				$bd=connexion();
 					
-					$req = $bd->prepare('DELETE FROM message WHERE id_message=:id');
+				$req = $bd->prepare('DELETE FROM message WHERE id_message=:id');
+				$req->bindParam(':id',$id);
 
-					$req->bindParam(':id',$id);
-
-					$req->execute();
+				$req->execute();
 				}
 
 ?>
