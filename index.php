29ba3f0ff22dacef2c64f1c7ebec90d334e3dc68
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-25 13:25:41 +0200

    propre

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php 
-	require_once("Accueil.php");
-	
+	require_once("view/accueil.php");	
 ?>
--- view/entete.php
+++ view/entete.php
@@ -6,7 +6,7 @@
 			<li><a href="Accueil.php">Accueil</a></li>
 			<?php if(isConnected() and isAdmin()){?><li><a href="Espace.php">Gestion</a></li><?php } ?>
 			<?php if(isConnected()){?><li><a href="Profil.php">Mes informations</a></li><?php } ?>
-			<?php if(isConnected()){?><li><a href="Activite.php">Activité</a></li><?php } ?>
+			<?php if(isConnected()){?><li><a href="view/activite.php">Activité</a></li><?php } ?>
 			<?php if(isConnected() and !isAdmin()){?><li><a href="Activite_Spec.php">Mes activités</a></li><?php } ?>
 			<?php if(!isConnected()){?><li><a href="Connexion.php">Connexion</a></li><?php } ?>
 			<?php if(!isConnected()){?><li><a href="Inscription.php">Inscription</a></li><?php } ?>
