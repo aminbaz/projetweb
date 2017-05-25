c176b9815b398d89336638725b6bbf5926c80621
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-25 22:32:41 +0200

    bd

--- controller/Controller_Page_Liste_Inscrit.php
+++ controller/Controller_Page_Liste_Inscrit.php
@@ -2,7 +2,7 @@
 	
 	require_once("model/Inscrire.php");
 	$idActivite = $_GET['id_activite'];
-	//$information=Liste_Inscription($idActivite);
+	$information=Liste_Inscription($idActivite);
 	require "view/liste_Inscrit.php";
 
 ?>
