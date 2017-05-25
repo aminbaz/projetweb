e9a7030b5d2ee86cae2429d25b6ea0b70a57e2e6
aminbaz <amin.bazaz@etu.umontpellier.fr>
2017-05-25 22:24:43 +0200

    problème fonction

--- controller/Controller_Page_Liste_Inscrit.php
+++ controller/Controller_Page_Liste_Inscrit.php
@@ -1,7 +1,7 @@
 <?php
 	
-	//require_once("model/Inscrire.php");
-	//$idActivite = $_GET['id_activite'];
+	require_once("model/Inscrire.php");
+	$idActivite = $_GET['id_activite'];
 	//$information=Liste_Inscription($idActivite);
 	require "view/liste_Inscrit.php";
 
